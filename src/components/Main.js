c80c6e8c7598df33ce368fc0bb58cc59cee03bd7
mpsinghji <Manpreet.singhcomet@gmail.com>
2024-04-30 12:11:56 +0530

    main page uodated

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,16 +3,21 @@ import data from './Data';
 import '../styles/Main.css';
 import Cards from './Cards';
 
-const Main = ({handleClick}) => {
+const Main = ({ handleClick }) => {
   return (
-    <section>
-        {
-            data.map((item)=>(
-                <Cards item={item} key={item.id} handleClick={handleClick}/>
-            ))
-        }
+    <section className='categories'>
+      {Object.keys(data).map((category) => (
+        <div className='category-container' key={category}>
+          <h1>{category}</h1>
+          <div className='category'>
+            {data[category].map((item) => (
+              <Cards item={item} key={item.id} handleClick={handleClick} />
+            ))}
+          </div>
+        </div>
+      ))}
     </section>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
